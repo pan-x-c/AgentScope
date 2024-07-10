c26920b6c12fd5744959bfd19faf1fee655571b9
github-action-benchmark <github@users.noreply.github.com>
2024-07-10 12:46:39 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 31a4539712cd911d2173848d112a9444e9e8fd5c

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1720615598823,
+  "repoUrl": "https://github.com/pan-x-c/AgentScope",
+  "entries": {
+    "Python Benchmark with pytest-benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "panxuchen.pxc@alibaba-inc.com",
+            "name": "panxuchen.pxc",
+            "username": "pan-x-c"
+          },
+          "committer": {
+            "email": "panxuchen.pxc@alibaba-inc.com",
+            "name": "panxuchen.pxc",
+            "username": "pan-x-c"
+          },
+          "distinct": true,
+          "id": "31a4539712cd911d2173848d112a9444e9e8fd5c",
+          "message": "add benchmark",
+          "timestamp": "2024-07-10T20:40:24+08:00",
+          "tree_id": "5e69cfd7a37ad27b8695046f4822f8d7703ed1e9",
+          "url": "https://github.com/pan-x-c/AgentScope/commit/31a4539712cd911d2173848d112a9444e9e8fd5c"
+        },
+        "date": 1720615577507,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "examples/distributed_simulation/bench.py::test_simulation",
+            "value": 0.07024748415595614,
+            "unit": "iter/sec",
+            "range": "stddev: 0.007868595342579682",
+            "extra": "mean: 14.235385252800006 sec\nrounds: 5"
+          }
+        ]
+      }
+    ]
+  }
+}
