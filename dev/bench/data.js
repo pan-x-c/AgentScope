f0623cc0339e3c0697e39bc58a40affafd2b15fb
github-action-benchmark <github@users.noreply.github.com>
2024-07-11 05:40:37 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 34391138aaa598eb1cb8bf30747f902193d82c26

---
+++ dev/bench/data.js
@@ -0,0 +1,35 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1720676437600,
+  "repoUrl": "https://github.com/pan-x-c/AgentScope",
+  "entries": {
+    "Python Benchmark with pytest-benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "pan-x-c",
+            "username": "pan-x-c"
+          },
+          "committer": {
+            "name": "pan-x-c",
+            "username": "pan-x-c"
+          },
+          "id": "34391138aaa598eb1cb8bf30747f902193d82c26",
+          "message": "test benchmark",
+          "timestamp": "2024-07-11T05:27:34Z",
+          "url": "https://github.com/pan-x-c/AgentScope/pull/6/commits/34391138aaa598eb1cb8bf30747f902193d82c26"
+        },
+        "date": 1720676436977,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "examples/distributed_simulation/bench.py::test_simulation",
+            "value": 0.07019008106603962,
+            "unit": "iter/sec",
+            "range": "stddev: 0.03474262540297959",
+            "extra": "mean: 14.247027283799998 sec\nrounds: 5"
+          }
+        ]
+      }
+    ]
+  }
+}
