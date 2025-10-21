66dfb1c8e96bf3a109a129fc60c90565e3805f14
pxc <panxuchen.pxc@alibaba-inc.com>
2025-10-21 10:28:28 +0800

    enable_thinking for qwen3

--- src/agentscope/model/_trinity_model.py
+++ src/agentscope/model/_trinity_model.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """A model class for RL Training with Trinity-RFT."""
 from typing import (
+    Optional,
     TYPE_CHECKING,
 )
 from ._openai_model import OpenAIChatModel
@@ -20,7 +21,8 @@ class TrinityChatModel(OpenAIChatModel):
         self,
         openai_async_client: AsyncOpenAI,
         generate_kwargs: dict[str, JSONSerializableObject] | None = None,
-    ):
+        enable_thinking: Optional[bool] = None,
+    ) -> None:
         """Initialize the Trinity model class.
 
         Args:
@@ -30,6 +32,9 @@ class TrinityChatModel(OpenAIChatModel):
             optional):
                 Additional keyword arguments to pass to the model's generate
                 method. Defaults to None.
+            enable_thinking (`bool`, optional):
+                Whether to enable the model's thinking capability. Only
+                applicable for Qwen3 series models. Defaults to None.
         """
         model_name = getattr(openai_async_client, "model_path", None)
         if model_name is None:
@@ -44,5 +49,15 @@ class TrinityChatModel(OpenAIChatModel):
             generate_kwargs=generate_kwargs,
             stream=False,  # RL training does not support streaming
         )
+        if enable_thinking is not None:
+            if "chat_template_kwargs" not in self.generate_kwargs:
+                self.generate_kwargs["chat_template_kwargs"] = {}
+            assert isinstance(
+                self.generate_kwargs["chat_template_kwargs"],
+                dict,
+            ), "chat_template_kwargs must be a dictionary."
+            self.generate_kwargs["chat_template_kwargs"][
+                "enable_thinking"
+            ] = enable_thinking
         # change the client instance to the provided one
         self.client = openai_async_client
--- tests/model_trinity_test.py
+++ tests/model_trinity_test.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# pylint: disable=too-many-statements
 """Unit tests for Trinity-RFT model class."""
 from unittest.async_case import IsolatedAsyncioTestCase
 from unittest.mock import Mock, AsyncMock
@@ -17,10 +18,28 @@ class TestTrinityModel(IsolatedAsyncioTestCase):
         mock_client.model_path = MODEL_NAME
 
         # test init
-        model = TrinityChatModel(openai_async_client=mock_client)
-        self.assertEqual(model.model_name, MODEL_NAME)
-        self.assertFalse(model.stream)
-        self.assertIs(model.client, mock_client)
+        model_1 = TrinityChatModel(
+            openai_async_client=mock_client,
+            enable_thinking=False,
+            generate_kwargs={
+                "temperature": 1.0,
+                "top_k": 2,
+            },
+        )
+        model_2 = TrinityChatModel(
+            openai_async_client=mock_client,
+            enable_thinking=True,
+            generate_kwargs={
+                "max_tokens": 500,
+                "top_p": 0.9,
+            },
+        )
+        self.assertEqual(model_1.model_name, MODEL_NAME)
+        self.assertFalse(model_1.stream)
+        self.assertIs(model_1.client, mock_client)
+        self.assertEqual(model_2.model_name, MODEL_NAME)
+        self.assertFalse(model_2.stream)
+        self.assertIs(model_2.client, mock_client)
 
         # create mock response
         messages = [{"role": "user", "content": "Hello"}]
@@ -43,11 +62,32 @@ class TestTrinityModel(IsolatedAsyncioTestCase):
             return_value=mock_response,
         )
 
-        result = await model(messages)
+        result = await model_1(messages)
         call_args = mock_client.chat.completions.create.call_args[1]
         self.assertEqual(call_args["model"], MODEL_NAME)
         self.assertEqual(call_args["messages"], messages)
         self.assertFalse(call_args["stream"])
+        self.assertFalse(call_args["chat_template_kwargs"]["enable_thinking"])
+        self.assertEqual(call_args["temperature"], 1.0)
+        self.assertEqual(call_args["top_k"], 2)
+        self.assertFalse("max_tokens" in call_args)
+        self.assertFalse("top_p" in call_args)
+        self.assertIsInstance(result, ChatResponse)
+        expected_content = [
+            TextBlock(type="text", text="Hi there!"),
+        ]
+        self.assertEqual(result.content, expected_content)
+
+        result = await model_2(messages)
+        call_args = mock_client.chat.completions.create.call_args[1]
+        self.assertEqual(call_args["model"], MODEL_NAME)
+        self.assertEqual(call_args["messages"], messages)
+        self.assertFalse(call_args["stream"])
+        self.assertTrue(call_args["chat_template_kwargs"]["enable_thinking"])
+        self.assertEqual(call_args["max_tokens"], 500)
+        self.assertEqual(call_args["top_p"], 0.9)
+        self.assertFalse("temperature" in call_args)
+        self.assertFalse("top_k" in call_args)
         self.assertIsInstance(result, ChatResponse)
         expected_content = [
             TextBlock(type="text", text="Hi there!"),
