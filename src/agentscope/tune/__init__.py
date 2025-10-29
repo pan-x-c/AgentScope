b1a6cc6956014bf77b41efe3b3f7a302e5a65bf9
pxc <panxuchen.pxc@alibaba-inc.com>
2025-10-29 17:44:51 +0800

    fix tests

--- src/agentscope/tune/__init__.py
+++ src/agentscope/tune/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 """The learning module of AgentScope, including RL and SFT."""
 
-from .tune import tune
-from .config import TuneConfig
-from .workflow import WorkflowType
+from ._tune import tune
+from ._config import TuneConfig
+from ._workflow import WorkflowType
 
 __all__ = [
     "tune",
